4515af87e4f9aa9a39f1623c23ecee407b59ea1e
Qualcomm Innovation Center <initial-contribution@quicinc.com>
2009-05-02 16:03:35 -0700

    Initial Contribution

--- gps/Android.mk
+++ gps/Android.mk
@@ -1,8 +1,14 @@
 # Use hardware GPS implementation if available.
 #
+
 ifneq ($(BOARD_GPS_LIBRARIES),)
   LOCAL_CFLAGS           += -DHAVE_GPS_HARDWARE
-  LOCAL_SHARED_LIBRARIES += $(BOARD_GPS_LIBRARIES)
+
+  ifneq (, $(filter msm7201a_surf msm7201a_ffa qsd8250_surf qsd8250_ffa, $(TARGET_PRODUCT)))
+  LOCAL_SHARED_LIBRARIES += librpc
+  endif
+
+  LOCAL_SHARED_LIBRARIES += $(BOARD_GPS_LIBRARIES) 
 endif
 
 # Use emulator GPS implementation if QEMU_HARDWARE is set.
