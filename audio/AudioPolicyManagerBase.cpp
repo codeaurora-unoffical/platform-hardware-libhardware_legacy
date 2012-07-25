8fd031d7acc249762dbf7063d11362f52bbe7928
Laxminath Kasam <lkasam@codeaurora.org>
2012-07-25 20:11:57 +0530

    libhardware: Fix Voip Downlink issue
    - In Voip call, Downlink path is not
    using MVS path.
    - In Voip usecase, Downlink path is
    not going through MVS and using normal
    playback path.
    This is due to failure in needsDirectOutput
    function. mPhoneState is not updated to
    MODE_IN_COMM by the time needsDirectOutput
    is called. This condition check is not
    required for Voip usecase.
    - Remove the condition for mPhoneState in
    needsDirectOutput which establishes the Voip
    Downlink path via MVS.
    
    CRs-Fixed: 381566
    (cherry picked from commit d8b8208ec872f180819cb8bd2f5c080da1bb4df8)
    
    Change-Id: I3a7b32dbb2b1ca7ac83371f7616330e592a37736

--- audio/AudioPolicyManagerBase.cpp
+++ audio/AudioPolicyManagerBase.cpp
@@ -2298,8 +2298,7 @@ bool AudioPolicyManagerBase::needsDirectOuput(AudioSystem::stream_type stream,
           (format !=0 && !AudioSystem::isLinearPCM(format)) ||
           ((stream == AudioSystem::VOICE_CALL)
           && (channels == AudioSystem::CHANNEL_OUT_MONO)
-          && ((samplingRate == 8000 )||(samplingRate == 16000 ))
-          && (mPhoneState == AudioSystem::MODE_IN_COMMUNICATION)));
+          && ((samplingRate == 8000 )||(samplingRate == 16000 ))));
 }
 
 uint32_t AudioPolicyManagerBase::getMaxEffectsCpuLoad()
