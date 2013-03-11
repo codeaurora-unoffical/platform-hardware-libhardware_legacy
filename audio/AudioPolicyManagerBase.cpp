65743477e302ae886ea946ef647760c203512a23
Vimal Puthanveed <vimalp@codeaurora.org>
2013-03-11 11:55:38 -0700

    Primary output check for sonification is removed.
    - When there are 3 different tracks (Ringtone, Music and Touchtone)
       the device switch happens for touchtones to primary if the primary
       output is active. This can lead to music being played in combo
       due to timing issue.
     - This is mainly because we have a check for, primary output if
       active we return true for SONIFICATION. This will set SONIFICATION
       for touch tones and this device switch will come after music device
       is set.
     - Hence music will start playing as SONIFICATION.
     - We have removed the primar output check for getting device strategy
    
    CRs-Fixed: 456842
    Change-Id: I86955b1d94724ab7eb574e7b897c9ad2ba54d899

--- audio/AudioPolicyManagerBase.cpp
+++ audio/AudioPolicyManagerBase.cpp
@@ -1984,7 +1984,6 @@ audio_devices_t AudioPolicyManagerBase::getNewDevice(audio_io_handle_t output, b
     audio_devices_t device = AUDIO_DEVICE_NONE;
 
     AudioOutputDescriptor *outputDesc = mOutputs.valueFor(output);
-    AudioOutputDescriptor *primaryOutputDesc = mOutputs.valueFor(mPrimaryOutput);
     // check the following by order of priority to request a routing change if necessary:
     // 1: the strategy enforced audible is active on the output:
     //      use device for strategy enforced audible
@@ -2003,8 +2002,7 @@ audio_devices_t AudioPolicyManagerBase::getNewDevice(audio_io_handle_t output, b
     } else if (isInCall() ||
                     outputDesc->isUsedByStrategy(STRATEGY_PHONE)) {
         device = getDeviceForStrategy(STRATEGY_PHONE, fromCache);
-    } else if (outputDesc->isUsedByStrategy(STRATEGY_SONIFICATION) ||
-               (primaryOutputDesc->isUsedByStrategy(STRATEGY_SONIFICATION))) {
+    } else if (outputDesc->isUsedByStrategy(STRATEGY_SONIFICATION)){
         device = getDeviceForStrategy(STRATEGY_SONIFICATION, fromCache);
     } else if (outputDesc->isUsedByStrategy(STRATEGY_SONIFICATION_RESPECTFUL)) {
         device = getDeviceForStrategy(STRATEGY_SONIFICATION_RESPECTFUL, fromCache);
