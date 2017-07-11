1eb4dd404ac86c6bbebd3d95ccd7c73267fe1d82
Rakesh Sunki <rsunki@codeaurora.org>
2017-07-10 17:30:56 -0700

    [AWARE]: Add support to config NAN discovery beacon interval
    Add support to configure discovery beacon interval in NAN enable
    and config request.
    
    Change-Id: I44d1918f740b35e39d751f26a5b5e4ee9b371a84
    CRs-Fixed: 2087449

--- include/hardware_legacy/wifi_nan.h
+++ include/hardware_legacy/wifi_nan.h
@@ -973,6 +973,12 @@ typedef struct {
     */
     u8 config_subscribe_sid_beacon;
     u32 subscribe_sid_beacon_val; // default value 0x0
+    /*
+       Discovery Beacon Interval config.
+       Default value is 128 msecs.
+    */
+    u8 config_discovery_beacon_int;
+    u32 discovery_beacon_interval;
 } NanEnableRequest;
 
 /*
@@ -1421,6 +1427,12 @@ typedef struct {
     */
     u8 config_subscribe_sid_beacon;
     u32 subscribe_sid_beacon_val; // default value 0x0
+    /*
+       Discovery Beacon Interval config.
+       Default value is 128 msecs.
+    */
+    u8 config_discovery_beacon_int;
+    u32 discovery_beacon_interval;
 } NanConfigRequest;
 
 /*
